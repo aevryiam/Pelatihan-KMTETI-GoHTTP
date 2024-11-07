7fe1f4e2a07a24e67e0da6eddcf434184ba6a13d
aevryiam <ilhamyusufwiam@mail.ugm.ac.id>
2024-11-07 10:02:47 +0700

    Update service.go

--- src/service/service.go
+++ src/service/service.go
@@ -7,7 +7,6 @@ import (
     "Pelatihan-KMTETI-GoHTTP/src/models"
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
-    //"go.mongodb.org/mongo-driver/mongo/options"
     "context"
 )
 
@@ -20,7 +19,6 @@ func init() {
     employeeCollection = db.Client.Database("go-http-server").Collection("employees")
 }
 
-// GetAllBooks retrieves all books
 func GetAllBooks(w http.ResponseWriter, r *http.Request) {
     var books []models.Book
     cursor, err := bookCollection.Find(context.TODO(), bson.D{})
@@ -40,27 +38,22 @@ func GetAllBooks(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(books)
 }
 
-// GetBookDetail retrieves a specific book by ID
 func GetBookDetail(w http.ResponseWriter, r *http.Request) {
     // Implement the logic to get book details by ID
 }
 
-// AddBook adds a new book to the database
 func AddBook(w http.ResponseWriter, r *http.Request) {
     // Implement the logic to add a new book
 }
 
-// UpdateBook updates stock and price of a book
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
     // Implement the logic to update stock and price of a book
 }
 
-// DeleteBook deletes a specific book by ID
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
     // Implement the logic to delete a book by ID
 }
 
-// GetAllEmployees retrieves all employees
 func GetAllEmployees(w http.ResponseWriter, r *http.Request) {
     var employees []models.Employee
     cursor, err := employeeCollection.Find(context.TODO(), bson.D{})
@@ -80,7 +73,5 @@ func GetAllEmployees(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(employees)
 }
 
-// AddEmployee adds a new employee to the database
 func AddEmployee(w http.ResponseWriter, r *http.Request) {
-    // Implement the logic to add a new employee
 }
